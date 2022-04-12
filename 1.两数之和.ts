20f369c00521e4109d50b63488ae459c0a55dcd3
彭江 <jiang.peng@yottacloud.cn>
2022-04-12 10:52:40 +0800

    f[1]

--- 1.两数之和.ts
+++ 1.两数之和.ts
@@ -6,7 +6,16 @@
 
 // @lc code=start
 function twoSum(nums: number[], target: number): number[] {
-
+  const dict: Map<number, number> = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const v = nums[i];
+    const another = target - v;
+    if(dict.has(another)){
+      const j = dict.get(another);
+      return [i,j];
+    }
+    dict.set(v,i);
+  }
 };
 // @lc code=end
 
