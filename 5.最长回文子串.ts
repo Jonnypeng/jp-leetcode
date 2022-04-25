5624c0401e721ba7169c6fc236eee405fe15cb64
彭江 <jiang.peng@yottacloud.cn>
2022-04-25 14:43:45 +0800

    [+]增加go的环境

--- 5.最长回文子串.ts
+++ 5.最长回文子串.ts
@@ -8,50 +8,69 @@
 
 
 function longestPalindrome(s: string): string {
-  const store: string[][] = [];
-  let longAnswer:string = s.length === 1 ? s : '';
+  let longAnswer: string = s.length === 1 ? s : '';
+  const dataMap: Map<string, boolean> = new Map();
+  const n = s.length;
+  const isQ = n % 2 !== 0;
+  let left: number;
+  let right: number;
 
-  const getPalindrome = (str: string,l:number,r:number):boolean => {
-    if(r<=l){
+  if (isQ) {
+    left = ((n - 1) / 2) + 1;
+    right = ((n - 1) / 2) - 1;
+  } else {
+    left = Math.ceil((n - 1) / 2);
+    right = Math.floor((n - 1) / 2);
+  }
+
+  const getPalindrome = (str: string, preStr: string): boolean => {
+    const key = str;
+    let val;
+    if (dataMap.has(key)) {
+      val = dataMap.get(key);
+    }
+    if (val !== undefined) {
+      dataMap.set(preStr, val);
+      return val;
+    }
+    if (str.length === 1 || str === '') {
+      dataMap.set(preStr, true);
       return true;
     }
-    if(str[l]===str[r]){
-      return getPalindrome(str,l+1,r-1); 
-    }else{
+    if (str[0] === str[str.length - 1]) {
+      return getPalindrome(str.slice(1, str.length - 1), preStr);
+    } else {
+      dataMap.set(preStr, false);
       return false;
     }
   }
 
-  if(longAnswer){
-      return longAnswer;
+  if (longAnswer) {
+    return longAnswer;
   }
 
-  for (let i = 0; i < s.length; i++) {
-    store[i] = [];
-    for (let j = 0; j < s.length; j++) {
-      if(j<i){
-        store[i][j] = '';
-        continue; 
-      }
-      if(i===j){
-        store[i][j] = s[i]; 
+  for (let i = n-1; i >= 0; i--) {
+    for (let j = 0; j < n; j++) {
+      if (j < i) {
         continue;
       }
-      const tempStr = `${store[i][j-1]}${s[j]}`;
-      store[i][j] = tempStr;
-      if(getPalindrome(tempStr,0,tempStr.length-1)){
+      if (i === j) {
+        continue;
+      }
+      const tempStr = s.slice(i, j + 1);
+      if (getPalindrome(tempStr, tempStr)) {
         longAnswer = tempStr.length > longAnswer.length ? tempStr : longAnswer;
       }
     }
   }
 
-  if(!longAnswer){
-    longAnswer = s[0]; 
+  if (!longAnswer) {
+    longAnswer = s[0];
   }
 
   return longAnswer;
 };
 
-// longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth");
+ //longestPalindrome("ajgiljtperkvubjmdsefcylksrxtftqrehoitdgdtttswwttmfuvwgwrruuqmxttzsbmuhgfaoueumvbhajqsaxkkihjwevzzedizmrsmpxqavyryklbotwzngxscvyuqjkkaotitddlhhnutmotupwuwyltebtsdfssbwayuxrbgihmtphshdslktvsjadaykyjivbzhwujcdvzdxxfiixnzrmusqvwujjmxhbqbdpauacnzojnzxxgrkmupadfcsujkcwajsgintahwgbjnvjqubcxajdyyapposrkpqtpqfjcvbhlmwfutgognqxgaukpmdyaxghgoqkqnigcllachmwzrazwhpppmsodvxilrccfqgpkmdqhoorxpyjsrtbeeidsinpeyxxpsjnymxkouskyhenzgieybwkgzrhhrzgkwbyeigznehyksuokxmynjspxxyepnisdieebtrsjypxroohqdmkpgqfccrlixvdosmppphwzarzwmhcallcginqkqoghgxaydmpkuagxqngogtufwmlhbvcjfqptqpkrsoppayydjaxcbuqjvnjbgwhatnigsjawckjuscfdapumkrgxxznjozncauapdbqbhxmjjuwvqsumrznxiifxxdzvdcjuwhzbvijykyadajsvtklsdhshptmhigbrxuyawbssfdstbetlywuwputomtunhhlddtitoakkjquyvcsxgnzwtoblkyryvaqxpmsrmzidezzvewjhikkxasqjahbvmueuoafghumbszttxmquurrwgwvufmttwwstttdgdtioherqtftxrsklycfesdmjbuvkreptjligja");
 // @lc code=end
 
