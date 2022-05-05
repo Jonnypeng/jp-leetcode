08a593a91d54ef27a8e59c2ccda4e076d22bc33e
彭江 <jiang.peng@yottacloud.cn>
2022-05-05 16:36:07 +0800

    [*]完成整数转罗马数字

---
+++ 12.整数转罗马数字.ts
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=12 lang=typescript
+ *
+ * [12] 整数转罗马数字
+ */
+
+// @lc code=start
+
+
+function intToRoman(num: number): string {
+  let temp: number = num;
+  let res:string = "";
+
+  while (temp > 0) {
+    if (temp >= 1000) {
+      temp -= 1000;
+      res+="M";
+      continue;
+    }
+    if (temp >= 900) {
+      temp -= 900;
+      res+="CM";
+      continue;
+    }
+    if (temp >= 500) {
+      temp -= 500;
+      res+="D";
+      continue;
+    }
+    if (temp >= 400) {
+      temp -= 400;
+      res+="CD";
+      continue;
+    }
+    if (temp >= 100) {
+      temp -= 100;
+      res+="C";
+      continue;
+    }
+    if (temp >= 90) {
+      temp -= 90;
+      res+="XC";
+      continue;
+    }
+    if (temp >= 50) {
+      temp -= 50;
+      res+="L";
+      continue;
+    }
+    if (temp >= 40) {
+      temp -= 40;
+      res+="XL";
+      continue;
+    }
+    if (temp >= 10) {
+      temp -= 10;
+      res+="X";
+      continue;
+    }
+    if (temp >= 9) {
+      temp -= 9;
+      res+="IX";
+      continue;
+    }
+    if (temp >= 5) {
+      temp -= 5;
+      res+="V";
+      continue;
+    }
+    if (temp >= 4) {
+      temp -= 4;
+      res+="IV";
+      continue;
+    }
+    if (temp >= 2) {
+      temp -= 2;
+      res+="II";
+      continue;
+    }
+    if (temp >= 1) {
+      temp -= 1;
+      res+="I";
+    }
+  }
+
+  return res;
+};
+// @lc code=end
+
+intToRoman(1994);
