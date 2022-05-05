5f003981f95746e6bd076a4d472522533b773bc6
彭江 <jiang.peng@yottacloud.cn>
2022-05-05 09:56:27 +0800

    [+]增加了两数之和的golang

--- 1.两数之和.ts
+++ 1.两数之和.ts
@@ -7,15 +7,18 @@
 // @lc code=start
 function twoSum(nums: number[], target: number): number[] {
   const dict: Map<number, number> = new Map();
+  const res:number[] = [];
   for (let i = 0; i < nums.length; i++) {
     const v = nums[i];
     const another = target - v;
     if(dict.has(another)){
-      const j = dict.get(another);
-      return [i,j];
+      const j = dict.get(another) as number;
+      res.push(i,j);
+      break;
     }
     dict.set(v,i);
   }
+  return res;
 };
 // @lc code=end
 
