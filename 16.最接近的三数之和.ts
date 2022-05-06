9ff07626173c3264ce44b8551e1987da69685bf6
彭江 <jiang.peng@yottacloud.cn>
2022-05-06 13:49:20 +0800

    [*]完成三数之和最洁净target

---
+++ 16.最接近的三数之和.ts
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=16 lang=typescript
+ *
+ * [16] 最接近的三数之和
+ */
+
+// @lc code=start
+
+function getSum(nums: number[], target: number): number {
+  const sum = nums.reduce((x,y)=>x+y);
+  const res = Math.abs(target - sum);
+  return res;
+}
+
+function threeSumClosest(nums: number[], target: number): number {
+  let p: number = 1;
+  let res: number = Number.MAX_VALUE;
+  let x = 0, y = 0, z = 0;
+  let sum: number;
+  const n = nums.length - 1;
+  while (p < n) {
+    for (let i = p - 1; i >= 0; i -= 1) {
+      for (let j = p + 1; j <= n; j += 1) {
+        let a = nums[i];
+        let b = nums[p];
+        let c = nums[j];
+        const _res = getSum([a, b, c], target);
+        if (_res < res) {
+          res = _res;
+          x = i; 
+          y = p;
+          z = j; 
+        }
+      }
+    }
+    p += 1;
+  }
+  sum = nums[x] + nums[y] + nums[z];
+  return sum;
+};
+// @lc code=end
+
+// threeSumClosest([-1,2,1,-4], 1)
+// threeSumClosest([-3, -2, -5, 3, -4], -1) // -2
+// threeSumClosest([1,1,1,0], -100) // 2
+threeSumClosest([1, 2, 4, 8, 16, 32, 64, 128], 82)
