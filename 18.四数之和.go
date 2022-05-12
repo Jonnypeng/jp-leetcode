5b36f3c61aaa928d43a30d0588db234d65fc5c72
彭江 <jiang.peng@yottacloud.cn>
2022-05-12 16:04:32 +0800

    [*]完成四数之和的go

---
+++ 18.四数之和.go
@@ -0,0 +1,62 @@
+package main
+
+import "sort"
+
+/*
+ * @lc app=leetcode.cn id=18 lang=golang
+ *
+ * [18] 四数之和
+ */
+
+// @lc code=start
+func fourSum(nums []int, target int) [][]int {
+	var n int = len(nums)
+	var res [][]int = [][]int{}
+	sort.Ints(nums)
+
+	for i := 0; i < n-3; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		if nums[i]+nums[i+1]+nums[i+2]+nums[i+3] > target {
+			break
+		}
+		if nums[i]+nums[n-1]+nums[n-2]+nums[n-3] < target {
+			continue
+		}
+		for j := i + 1; j < n-2; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+			if nums[i]+nums[j]+nums[j+1]+nums[j+2] > target {
+				break
+			}
+			if nums[i]+nums[j]+nums[n-1]+nums[n-2] < target {
+				continue
+			}
+			left, right := j+1, n-1
+			for left < right {
+				sum := nums[i] + nums[j] + nums[left] + nums[right]
+				if sum == target {
+					res = append(res, []int{nums[i], nums[j], nums[left], nums[right]})
+					for left < right && nums[left] == nums[left+1] {
+						left += 1
+					}
+					left += 1
+					for left < right && nums[right] == nums[right-1] {
+						right -= 1
+					}
+					right -= 1
+				} else if sum < target {
+					left += 1
+				} else if sum > target {
+					right -= 1
+				}
+			}
+		}
+	}
+
+	return res
+}
+
+// @lc code=end
