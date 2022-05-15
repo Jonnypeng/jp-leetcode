b623d643116491da3013673149909dfe56b8fb30
彭江 <jiang.peng@yottacloud.cn>
2022-05-15 13:29:53 +0800

    [*]完成#25的tes

--- 25.k-个一组翻转链表.ts
+++ 25.k-个一组翻转链表.ts
@@ -1,9 +1,3 @@
-/*
- * @Author: 彭江
- * @Date: 2022-05-14 08:57:31
- * @LastEditors: 彭江
- * @LastEditTime: 2022-05-15 12:17:21
- */
 import { ListNode } from './type';
 
 /*
@@ -27,43 +21,52 @@ import { ListNode } from './type';
  * }
  */
 
-const swap = (aNode: ListNode, bNode: ListNode) => {
-  const startNext = aNode.next;
-  const endNext = bNode.next;
-  if (startNext === bNode) {
-    aNode.next = endNext;
-    bNode.next = aNode;
-  } else {
-    aNode.next = endNext;
-    bNode.next = startNext;
-  }
-  return startNext;
-}
+
 
 function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
-  if (!head) {
+  if (!head || head.next === null) {
     return head;
   }
 
-  let p:ListNode | null = head;
-  const nodesMap: Array<ListNode|null>[] = [];
+  // 可变的位置
+  let p: ListNode | null = head;
+  // 头节点
+  let root: ListNode | null  = null;
+
+  const nodesMap: Array<ListNode | null>[] = [];
 
   // 将K节点之前的所有节点都推送到数组中
   while (p !== null) {
-    let nodes:Array<ListNode|null> = [];
+    let nodes: Array<ListNode | null> = [];
     for (let i = 0; i < k; i += 1) {
-        nodes[k-i] = p;
-        p = p?.next ?? null;
-        if(i === k -1){
-          nodes = nodes.includes(null) ? nodes.reverse() : nodes;
-          nodesMap.push(nodes);
-        }
+      nodes[k - (i + 1)] = p;
+      p = p?.next ?? null;
+      if (i === k - 1) {
+        nodes = nodes.includes(null) ? nodes.reverse() : nodes;
+        nodesMap.push(nodes);
+      }
     }
   }
 
-  console.log(nodesMap);
+  p = head;
 
-  return head;
+  // 遍历已排序的二维数组，将节点重新拼接
+  for (let i = 0; i < nodesMap.length; i += 1) {
+    for (let j = 0; j < nodesMap[i].length; j += 1) {
+      if (nodesMap[i][j]) {
+        (nodesMap[i][j] as ListNode).next = null;
+        if (i === 0 && j === 0) {
+          root = nodesMap[i][j];
+        }
+        if (p) {
+          p.next = nodesMap[i][j];
+          p = nodesMap[i][j];
+        }
+      }
+    }
+  }
+
+  return root;
 
 
 
@@ -83,12 +86,12 @@ const l1: ListNode = {
         next: {
           val: 5,
           next: {
-            val:6,
-            next:{
-              val:7,
-              next:{
-                val:8,
-                next:null
+            val: 6,
+            next: {
+              val: 7,
+              next: {
+                val: 8,
+                next: null
               }
             }
           }
@@ -101,5 +104,5 @@ const l1: ListNode = {
 const k1 = 2;
 const k2 = 3;
 
-const r = reverseKGroup(l1, 3)
+const r = reverseKGroup(l1, 5)
 console.log(r);
