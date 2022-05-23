84ff7d0eb7e9a69d630e9bb1cd610c25090b9d0e
彭江 <jiang.peng@yottacloud.cn>
2022-05-23 10:11:25 +0800

    [+]finshed #39

--- 39.组合总和.ts
+++ 39.组合总和.ts
@@ -6,7 +6,26 @@
 
 // @lc code=start
 function combinationSum(candidates: number[], target: number): number[][] {
+  const arr: number[][] = [];
 
+  const dfs = (target: number, combin: number[], index: number) => {
+    if (index > candidates.length - 1) {
+      return;
+    }
+    if (target === 0) {
+      arr.push(combin);
+      return;
+    }
+    if (target - candidates[index] >= 0) {
+      dfs(target - candidates[index], [...combin, candidates[index]], index);
+    }
+    dfs(target, combin, index + 1);
+  }
+
+  dfs(target, [], 0);
+
+  return arr;
 };
+
 // @lc code=end
 
