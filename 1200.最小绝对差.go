3aa3a01e2f51803f325852e63221b8d710475744
彭江 <jiang.peng@yottacloud.cn>
2022-07-04 14:02:37 +0800

    [+]finshed #1200

---
+++ 1200.最小绝对差.go
@@ -0,0 +1,47 @@
+package leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+// import (
+// 	"math"
+// 	"sort"
+// )
+
+/*
+ * @lc app=leetcode.cn id=1200 lang=golang
+ *
+ * [1200] 最小绝对差
+ */
+// @lc code=start
+func minimumAbsDifference(arr []int) [][]int {
+	res := [][]int{}
+	n := len(arr)
+	minOffSet := math.MaxInt
+	sort.Ints(arr)
+
+	for i, j := 0, 1; j < n; {
+		minOffSet = int(math.Min(float64(minOffSet), float64(arr[j]-arr[i])))
+		i++
+		j++
+	}
+
+	for i, j := 0, 1; j < n; {
+		if arr[j]-arr[i] == minOffSet {
+			res = append(res, []int{arr[i], arr[j]})
+		}
+		i++
+		j++
+	}
+
+	return res
+
+}
+
+// @lc code=end
+
+// func main() {
+// 	minimumAbsDifference([]int{1, 3, 6, 10, 15})
+// }
