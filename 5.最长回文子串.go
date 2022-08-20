9dd70a6afe1f110f490bc03cae5ab6525adb5c48
彭江 <jiang.peng@yottacloud.cn>
2022-08-20 11:35:34 +0800

    [*]完成#654

--- 5.最长回文子串.go
+++ 5.最长回文子串.go
@@ -1,3 +1,4 @@
+package main
 
 /*
  * @lc app=leetcode.cn id=5 lang=golang
---
+++ 654.最大二叉树.go
@@ -0,0 +1,58 @@
+package main
+
+import "math"
+
+/*
+ * @lc app=leetcode.cn id=654 lang=golang
+ *
+ * [654] 最大二叉树
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func makeNode(nums *[]int) *TreeNode {
+	// 当数组的值为0，则返回空节点
+	if len(*nums) == 0 {
+		return nil
+	}
+	// 找到最大值，左数组，有数组
+	maxVal, leftArr, rightArr := findMaxVal(nums)
+	return &TreeNode{
+		Val:   maxVal,
+		Left:  makeNode(&leftArr),
+		Right: makeNode(&rightArr),
+	}
+}
+
+// 查询 最大值，左数组，有数组
+func findMaxVal(nums *[]int) (maxVal int, leftArr []int, rightArr []int) {
+	val := math.MinInt
+	index := -1
+	for i, v := range *nums {
+		if v > val {
+			val = v
+			index = i
+		}
+	}
+	return val, (*nums)[:index], (*nums)[index+1:]
+}
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	return makeNode(&nums)
+}
+
+// @lc code=end
