eaa27edd03645368c8d4b91cbde6b3ef9b9911cd
彭江 <jiang.peng@yottacloud.cn>
2022-09-04 11:59:36 +0800

    [+]准备完成6188

--- 6169.最长优雅子数组.ts
+++ 6169.最长优雅子数组.ts
@@ -59,6 +59,6 @@ function longestNiceSubarray(nums: number[]): number {
 	return res;
 };
 
-longestNiceSubarray([1, 3, 8, 48, 10]);
+// longestNiceSubarray([1, 3, 8, 48, 10]);
 // longestNiceSubarray([84139415, 693324769, 614626365, 497710833, 615598711, 264, 65552, 50331652, 1, 1048576, 16384, 544, 270532608, 151813349, 221976871, 678178917, 845710321, 751376227, 331656525, 739558112, 267703680]); //8 
 
