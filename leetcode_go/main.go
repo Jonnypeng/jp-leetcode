fb8e116cff0616b062437c3962f339c08481fcad
彭江 <jiang.peng@yottacloud.cn>
2022-09-11 22:16:38 +0800

    sync

--- leetcode_go/main.go
+++ leetcode_go/main.go
@@ -3,7 +3,8 @@ package main
 import "leetcode/pkg/algorithm"
 
 func main() {
-	algorithm.PartitionString("gizfdfri") // 2
+	algorithm.MincostToHireWorkers([]int{10, 20, 5}, []int{70, 50, 30}, 2)
+	// algorithm.PartitionString("gizfdfri") // 2
 	// algorithm.MostFrequentEven([]int{29, 47, 21, 41, 13, 37, 25, 7})
 	// algorithm.Jump([]int{2, 3, 1}) //1
 	// algorithm.IsPalindrome("abcde")
