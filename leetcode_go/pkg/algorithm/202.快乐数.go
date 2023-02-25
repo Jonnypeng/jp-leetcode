971b5e5a5251b259fd084e50c84669a41f1060aa
彭江 <jiang.peng@yottacloud.cn>
2023-02-25 17:04:00 +0800

    ss

--- leetcode_go/pkg/algorithm/202.快乐数.go
+++ leetcode_go/pkg/algorithm/202.快乐数.go
@@ -13,7 +13,7 @@ func isHappy(n int32) bool {
 	// 迭代器的条件是此数没有被循环过，且不为1
 	for !m[n] && n != 1 {
 		m[n] = true   // 进行已循环标记
-		n = getAns(n) // 计算出一个平方数，并赋值给新的数
+		n = getAns(n) // 计算出一个平方数，并赋值给新的数·
 	}
 
 	return n == 1 // 比较此数是不是1
@@ -23,7 +23,7 @@ func getAns(n int32) int32 {
 	var val int32
 	for n > 0 {
 		c := n % 10  // 取出最后一位数
-		val += c * c // 计算平方
+		val += c * c // 计算平方·
 		n /= 10      // 丢掉最后一位数
 	}
 	return val
@@ -31,6 +31,6 @@ func getAns(n int32) int32 {
 
 // @lc code=end
 
-func IsHappy(n int32) bool {
+func IsHappy(n int32) bool {·
 	return isHappy(n)
 }
